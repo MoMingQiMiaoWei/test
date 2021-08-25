d9c2e82651bb7319cbd794a3e8ffb39f7091c2a4
MoMingQiMiaoWei <2056761763@qq.com>
2021-08-25 17:19:27 +0800

    8_25动态版通讯录实现

---
+++ tset_dynamic_contast/test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include "contact.h"
+
+
+
+void menu()
+{
+	printf("*******************************\n");
+	printf("***** 1. add     2.del    *****\n");
+	printf("***** 3. serch   4.modify *****\n");
+	printf("***** 5. show    6.sort   *****\n");
+	printf("***** 0.exit              *****\n");
+	printf("*******************************\n");
+}
+
+int main()
+{
+	int input = 0;
+	//创建通讯录
+	struct Contact con;//con是通讯录，包含：data指针,size和capacity
+	//初始化通讯录
+	InitContact(&con);
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case MODIFY:
+			ModifyContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case SORT:
+			SortContact(&con);
+			break;
+		case EXIT:
+			DestroyContact();//销毁通讯录，释放 malloc()开辟的空间
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+        }
+	} while (input);
+	
+	return 0;
+}
+
